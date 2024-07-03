6d9f94fa37a8cded3f32f893cd945e498209f314
sri.karan <sri.karan@blackstraw.ai>
2024-07-03 18:00:43 +0530

    fastAPI

--- MLOps/energy-forecasting/feature-pipeline/feature_pipeline/etl/validation.py
+++ MLOps/energy-forecasting/feature-pipeline/feature_pipeline/etl/validation.py
@@ -38,13 +38,13 @@ def build_expectation_suite() -> ExpectationSuite:
         )
     )
 
-    # # Area
-    # expectation_suite_energy_consumption.add_expectation(
-    #     ExpectationConfiguration(
-    #         expectation_type="expect_column_distinct_values_to_be_in_set",
-    #         kwargs={"column": "area", "value_set": set([0, 1, 2])},
-    #     )
-    # )
+    # Area
+    expectation_suite_energy_consumption.add_expectation(
+        ExpectationConfiguration(
+            expectation_type="expect_column_distinct_values_to_be_in_set",
+            kwargs={"column": "area", "value_set": set(0, 1, 2)},
+        )
+    )
     expectation_suite_energy_consumption.add_expectation(
         ExpectationConfiguration(
             expectation_type="expect_column_values_to_be_of_type",
@@ -52,55 +52,55 @@ def build_expectation_suite() -> ExpectationSuite:
         )
     )
 
-    # # Consumer type
-    # expectation_suite_energy_consumption.add_expectation(
-    #     ExpectationConfiguration(
-    #         expectation_type="expect_column_distinct_values_to_be_in_set",
-    #         kwargs={
-    #             "column": "consumer_type",
-    #             "value_set": (
-    #                 111,
-    #                 112,
-    #                 119,
-    #                 121,
-    #                 122,
-    #                 123,
-    #                 130,
-    #                 211,
-    #                 212,
-    #                 215,
-    #                 220,
-    #                 310,
-    #                 320,
-    #                 330,
-    #                 340,
-    #                 350,
-    #                 360,
-    #                 370,
-    #                 381,
-    #                 382,
-    #                 390,
-    #                 410,
-    #                 421,
-    #                 422,
-    #                 431,
-    #                 432,
-    #                 433,
-    #                 441,
-    #                 442,
-    #                 443,
-    #                 444,
-    #                 445,
-    #                 446,
-    #                 447,
-    #                 450,
-    #                 461,
-    #                 462,
-    #                 999,
-    #             ),
-    #         },
-    #     )
-    # )
+    # Consumer type
+    expectation_suite_energy_consumption.add_expectation(
+        ExpectationConfiguration(
+            expectation_type="expect_column_distinct_values_to_be_in_set",
+            kwargs={
+                "column": "consumer_type",
+                "value_set": (
+                    111,
+                    112,
+                    119,
+                    121,
+                    122,
+                    123,
+                    130,
+                    211,
+                    212,
+                    215,
+                    220,
+                    310,
+                    320,
+                    330,
+                    340,
+                    350,
+                    360,
+                    370,
+                    381,
+                    382,
+                    390,
+                    410,
+                    421,
+                    422,
+                    431,
+                    432,
+                    433,
+                    441,
+                    442,
+                    443,
+                    444,
+                    445,
+                    446,
+                    447,
+                    450,
+                    461,
+                    462,
+                    999,
+                ),
+            },
+        )
+    )
     expectation_suite_energy_consumption.add_expectation(
         ExpectationConfiguration(
             expectation_type="expect_column_values_to_be_of_type",
